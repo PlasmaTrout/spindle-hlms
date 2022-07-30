6d5b4eb92161ed8281b61ce8a906911e2c3a9bdb
Jeff Downing <jeff.downing@intevity.com>
2022-07-30 18:08:06 -0400

    Added photo support to make it look more professional and ExtJs like

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,11 @@
 export default function NavBar() {
   return(
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="http://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
-          width="112"
-          height="28"
-        />
+    <nav className="nav-group">
+      <h5 className="nav-group-title">Navigation</h5>
+      <a className="nav-group-item active">
+        <span class="icon icon-home"></span>
+        Alarms
       </a>
-    </div>
-  </nav>
+    </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,64 @@
 import Head from "next/head";
-import NavBar from '../components/navbar';
+import NavBar from "../components/navbar";
 import AlarmTable from "../components/alarmtable";
 import { useEffect } from "react";
-import io from 'socket.io-client'
+import io from "socket.io-client";
 let socket;
 
 export default function Home() {
-
   useEffect(() => socketInitializer(), []);
 
   const socketInitializer = async () => {
-    await fetch('/api/socket')
-    socket = io()
+    await fetch("/api/socket");
+    socket = io();
 
-    socket.on('connect', () => {
+    socket.on("connect", () => {
       console.log("Socket connected!");
-     
     });
 
     return null;
-  }
+  };
 
   return (
-    <div className="container">
+    <div>
       <Head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
-        ></meta> 
+        ></meta>
         <title>Spindle NMS</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
+        <link rel="stylesheet" href="../photon/css/photon.min.css" />
         <link rel="stylesheet" href="/alarmtable.css"></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-       <NavBar></NavBar>
-       <AlarmTable></AlarmTable>
-        <div className="container"></div>
+        <div className="window">
+          <header className="toolbar toolbar-header">
+            <h1 className="title">Spindle NMS</h1>
+            <div className="toolbar-actions">
+              <div className="btn-group">
+                
+              </div>
+              <button className="btn btn-default pull-right">
+                  <span className="icon icon-retweet"></span>
+                </button>
+            </div>
+          </header>
+          <div className="window-content">
+            <div className="pane-group">
+              <div class="pane-sm sidebar">
+                <NavBar></NavBar>
+              </div>
+              <div class="pane">
+                <AlarmTable></AlarmTable>
+              </div>
+            </div>
+          </div>
+          <footer class="toolbar toolbar-footer">
+            <h1 class="title">Footer</h1>
+          </footer>
+        </div>
       </main>
 
       <footer></footer>
