bf05a456b83b32e9078427f38070611bca68e6fe
Jeff Downing <jeff.downing@intevity.com>
2023-10-31 16:10:33 -0400

    Adding dockerization stuff

---
+++ src/main/java/com/programmervsworld/Spindle NMS2Configuration.java
@@ -0,0 +1,10 @@
+package com.programmervsworld;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import jakarta.validation.constraints.*;
+
+public class Spindle NMS2Configuration extends Configuration {
+    // TODO: implement service configuration
+}
